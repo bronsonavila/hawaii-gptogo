28e48e2897e73b4ff80fa6818bb4722dabb6691d
Bronson Avila <bronson.avila@gmail.com>
2025-05-04 18:28:11 -1000

    Modify theme-provider

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -1,9 +1,9 @@
 'use client'
 
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import React from 'react'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 
 const theme = createTheme({ palette: { mode: 'dark' } })
 
