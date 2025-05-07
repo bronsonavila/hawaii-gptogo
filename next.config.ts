e1917a3857d8cb4b1986a85ca16ed33f6da2c00b
Bronson Avila <bronson.avila@gmail.com>
2025-05-07 00:16:10 -1000

    Fix basePath configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/hawaii-gptogo'
-  /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'export'
 }
 
 export default nextConfig
